fcbe3695ca917ca8c1e38b196bafe3ff1d4a9024
ArmanSaffari <a.saffari256@gmail.com>
2023-07-06 14:20:09 -0400

    new images and icons added

---
+++ src/components/Carousel.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: [
+    "imagesLink"
+    ],
+  data() {
+    return {
+      currentImageIndex: 0
+    };
+  },
+  methods: {
+    nextImage() {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.imagesLink.length;
+    },
+    previousImage() {
+      this.currentImageIndex = (this.currentImageIndex - 1 + this.imagesLink.length) % this.imagesLink.length;
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="relative rounded-lg shadow-lg inset-0 w-full overflow-hidden">
+    <div class="absolute w-full h-full flex justify-between items-center p-3">
+      <button @click="previousImage" class="flex justify-center items-center hover:bg-black/20 h-10 w-10 rounded-full">
+        <img src="../assets/icons/prev2.svg" />
+      </button>
+      <button @click="nextImage" class="flex justify-center items-center hover:bg-black/20 h-10 w-10 rounded-full">
+        <img src="../assets/icons/next2.svg" />
+      </button>
+    </div>
+    <img :src="imagesLink[currentImageIndex]" class="w-full h-auto" />
+  </div>
+</template>
--- src/components/ProjectModal.vue
+++ src/components/ProjectModal.vue
@@ -1,5 +1,10 @@
 <script>
+import Carousel from "./Carousel.vue"
+
 export default {
+  components: {
+    Carousel
+  },
   data() {
     return {
       isOpen: false
@@ -24,30 +29,49 @@ export default {
   <transition name="modal">
     <div class="fixed inset-0 flex items-center justify-center z-50">
       <div class="fixed inset-0 bg-black opacity-50" @click = "closeModal"></div>
-      <div class="bg-slate-950 border boredr-white relative rounded-lg shadow-lg p-4 md:w-1/2 w-full">
+      <div class="bg-slate-950 border boredr-white relative rounded-lg shadow-lg p-4 md:w-1/2 w-full overflow-y-auto max-h-screen">
         <button @click="closeModal" class="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
           <img src="../assets/icons/close.svg" />
         </button>
         <div>
           <div class="p-2">
-            <img :src="viewingProject.bannerUrl"
-            :alt="viewingProject.title"
-            class="opacity-50 hover:opacity-100"/>
-            <h1 class="text-xl font-fahkwang text-center font-outline-white">{{ viewingProject.title }} </h1>
-            <ul class="m-4">
-              <li v-for="feature in viewingProject.features"
-              class="text-white text-xs list-disc">
-              {{ feature }}
-              </li>
-            </ul>
-          </div>
-          <div class="flex">
-            <div class="w-1/2" v-for="tech in viewingProject.technologies">
-              <img :alt="tech.name"
-              class="h-14 inline rounded-lg"
-              :src="getImageUrl(tech.icon)" />
+            <h1 class="text-2xl font-fahkwang text-center font-outline-white">{{ viewingProject.title }} </h1>
+            <div class="flex flex-wrap">
+              <Carousel :imagesLink="[viewingProject.bannerUrl]"></Carousel>
+              <!-- <img :src="viewingProject.bannerUrl"
+              :alt="viewingProject.title"
+              class="my-2 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-cyan-500/50"/> -->
+              <div>
+                <h3 class="text-lg text-white font-semibold">Features:</h3>
+                <ul class="mx-4">
+                  <li v-for="feature in viewingProject.features"
+                  class="text-slate-300 text-xs list-disc">
+                  {{ feature }}
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
+          <div class="w-full p-2">
+            <h3 class="text-lg text-white font-semibold">Technologies Used:</h3>
+            <div v-for="tech in viewingProject.technologies"
+              class="w-auto flex border border-white m-2 p-2 rounded-lg 
+              hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
+              hover:shadow-lg hover:shadow-cyan-500/50 hover:text-white" >
+              <div class=" bg-white rounded-lg h-20 aspect-square p-1 flex items-center justify-center">
+                <img :alt="tech.name"
+                class="object-contain	"
+                :src="getImageUrl(tech.icon)" />
+              </div>
+              <div class="text-slate-300 px-2">
+                <h3 class="text-sm font-bold">{{ tech.name }}</h3>
+                <p class="text-xs">{{ tech.description }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-2">
+            
+            </div>
         </div>
         
       </div>

ffecfd70808e50abe3d3d07bc93f1e19ac5acd24
ArmanSaffari <a.saffari256@gmail.com>
2023-07-11 00:22:04 -0400

    skills and projects sections modified

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -37,18 +37,20 @@ import Carousel from "./Carousel.vue"
       Projects
     </h3>
     <div class="left-0 w-1/2 h-2 bg-gradient-to-r from-teal-500 from-30%  to-blue-700 to-90%"></div>
-    <div class="w-full pb-4">
+    <div class="bg-white">
+      <img width="50" height="50" :src="getImageUrl('down.svg')"/>
+    </div>
+    <div class="w-full pb-4 flex flex-wrap">
       <div
         v-for="project in projectsArray" :key="project.id"
-        class="flex flex-wrap justify-start items-center p-4 mt-12 w-full
-          border border-white rounded-lg shadow text-white hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
+        class="flex flex-wrap justify-start items-start p-4 mt-12 w-full
+          border border-white rounded-lg shadow text-white
           hover:shadow-lg hover:shadow-cyan-500/50"
+          
         :class="{ hidden: ![1, 2, 3, 4, 5].includes( project.id ) }"
       >
-        <div class="relative w-full m-0 overflow-hidden xl:w-full">
-          <Carousel :imagesLink="project.bannerUrl" />
-        </div>
-        <div class="w-full p-2 xl:w-full">
+        
+      <div class="w-full p-2 xl:w-1/2 xl:pr-2">
           <h5 class="text-2xl font-fahkwang font-bold">
             {{ project.title }}
           </h5>
@@ -59,20 +61,28 @@ import Carousel from "./Carousel.vue"
               :src="getImageUrl(`${lang}.svg`)"
             />
           </div>
+          
+        </div>
+
+        <div class="relative w-full m-0 overflow-hidden xl:w-1/2">
+          <Carousel :imagesLink="project.bannerUrl" />
+        </div>
+
+        <div class="w-full">
           <div class="projectDescription">
             <h4 class="text-md font-semibold">Description:</h4>
-            <p class="text-md">{{ project.briefDescription }}</p>
+            <p class="text-md pl-2">{{ project.briefDescription }}</p>
           </div>
           <div class="projectLinks">
             <h4 class="my-2 font-semibold text-md">View The Project Here:</h4>
-            <div class="flex gap-2 flex-wrap">
+            <div class="flex gap-2 flex-wrap pl-2">
               <a
                 v-for="link in project.links"
                 target="_blank"
                 :href="link.url"
                 class="flex items-center border-2 border-white hover:bg-white hover:text-black p-1 rounded-lg text-bold group"
               >
-                <div class="flex items-center gap-1 ">
+                <div class="flex items-center gap-1">
                   <span class="text-sm font-semibold">{{ link.name }}</span>
                   <div class="inline relative m-0 p-0 h-5 w-5">
                     <img class=" absolute top-0 left-0 h-5 w-5 group-hover:hidden"
@@ -84,10 +94,11 @@ import Carousel from "./Carousel.vue"
               </a>
             </div>
           </div>
-          <div class="projectFeatures py-2 hover:font-bold">
+          <div class="projectFeatures py-2 hover:font-bold pl-">
             <button type="button" @click = "showProjectModal(project.id)">See All Features Here!</button>
           </div>
         </div>
+
       </div>
     </div>
   </div>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -31,7 +31,7 @@
       class="relative left-1/2 w-7/12 mb-6 bg-gradient-to-l from-teal-500 from-30%  to-blue-700 to-90% h-2"
     ></div>
     <div class="w-full flex flex-wrap items-stretch	">
-      <div class="my-4 p-2 w-full md:w-1/3 ">
+      <div class="my-4 p-2 w-full md:w-1/2 2xl:w-1/4">
         <div
           class="relative p-8 w-full h-full border border-gray-400 rounded-lg 
           hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
@@ -43,17 +43,21 @@
             Front End
           </h5>
           <div class="flex gap-4 justify-center flex-wrap"> 
-            <img
-            v-for="skill in skillsArray.frontEnd" :key="skill.id"
-            :alt="skill.name"
-            class="h-14 inline rounded-lg"
-            v-bind:src="getImageUrl(skill.imgFileName)"
-            />
+            <div class="relative group"
+              v-for="skill in skillsArray.frontEnd" >
+              <img
+              :key="skill.id"
+              :alt="skill.name"
+              class="h-14 inline rounded-lg"
+              v-bind:src="getImageUrl(skill.imgFileName)"
+              />
+              <div class="absolute hidden group-hover:block top-1/2 left-1/2 z-50 bg-white text-slate-950 rounded-lg px-2 text-center font-semibold">{{ skill.name }}</div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="my-4 p-2 w-full md:w-1/3 ">
+      <div class="my-4 p-2 w-full md:w-1/2 2xl:w-1/4 ">
         <div
           class="relative p-8 w-full h-full border border-gray-400 rounded-lg 
           hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
@@ -65,17 +69,21 @@
             Back End
           </h5>
           <div class="flex gap-4 justify-center flex-wrap"> 
-            <img
-            v-for="skill in skillsArray.backEnd" :key="skill.id"
-            :alt="skill.name"
-            class="h-14 inline rounded-lg"
-            v-bind:src="getImageUrl(skill.imgFileName)"
-            />
+            <div class="relative group"
+              v-for="skill in skillsArray.backEnd" >
+              <img
+              :key="skill.id"
+              :alt="skill.name"
+              class="h-14 inline rounded-lg"
+              v-bind:src="getImageUrl(skill.imgFileName)"
+              />
+              <div class="absolute hidden group-hover:block top-1/2 left-1/2 z-50 bg-white text-slate-950 rounded-lg px-2 text-center font-semibold">{{ skill.name }}</div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="my-4 p-2 w-full md:w-1/3">
+      <div class="my-4 p-2 w-full md:w-1/2 2xl:w-1/4">
         <div
           class="relative p-8 w-full h-full border border-gray-400 rounded-lg 
           hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
@@ -87,17 +95,21 @@
             Design
           </h5>
           <div class="flex gap-4 justify-center flex-wrap"> 
-            <img
-            v-for="skill in skillsArray.design" :key="skill.id"
-            :alt="skill.name"
-            class="h-14 inline rounded-lg"
-            v-bind:src="getImageUrl(skill.imgFileName)"
-            />
+            <div class="relative group"
+              v-for="skill in skillsArray.design" >
+              <img
+              :key="skill.id"
+              :alt="skill.name"
+              class="h-14 inline rounded-lg"
+              v-bind:src="getImageUrl(skill.imgFileName)"
+              />
+              <div class="absolute hidden group-hover:block top-1/2 left-1/2 z-50 bg-white text-slate-950 rounded-lg px-2 text-center font-semibold">{{ skill.name }}</div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="my-4 p-2 w-full md:w-1/3">
+      <div class="my-4 p-2 w-full md:w-1/2 2xl:w-1/4">
         <div
           class="relative p-8 w-full h-full border border-gray-400 rounded-lg 
           hover:bg-gradient-to-br from-teal-500 from-20% to-blue-700 to-80%
@@ -109,12 +121,16 @@
             Other
           </h5>
           <div class="flex gap-4 justify-center flex-wrap"> 
-            <img
-            v-for="skill in skillsArray.other" :key="skill.id"
-            :alt="skill.name"
-            class="h-14 inline rounded-lg"
-            v-bind:src="getImageUrl(skill.imgFileName)"
-            />
+            <div class="relative group"
+              v-for="skill in skillsArray.other" >
+              <img
+              :key="skill.id"
+              :alt="skill.name"
+              class="h-14 inline rounded-lg"
+              v-bind:src="getImageUrl(skill.imgFileName)"
+              />
+              <div class="absolute hidden group-hover:block top-1/2 left-1/2 z-50 bg-white text-slate-950 rounded-lg px-2 text-center font-semibold">{{ skill.name }}</div>
+            </div>
           </div>
         </div>
       </div>
